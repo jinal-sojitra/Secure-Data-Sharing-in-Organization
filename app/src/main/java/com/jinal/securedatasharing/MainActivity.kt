9132e8a938a009c6144d9841aba830a5f77e484b
JINAL <jinalsojitra552@gmail.com>
2022-03-17 11:28:28 +0530

    Login Activity Design(activity_main.xml) has opened successfully!!

--- app/src/main/java/com/jinal/securedatasharing/MainActivity.kt
+++ app/src/main/java/com/jinal/securedatasharing/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.jinal.securedatasharing
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val login_text = findViewById<TextView>(R.id.tv_login)
+        login_text.setOnClickListener {
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
